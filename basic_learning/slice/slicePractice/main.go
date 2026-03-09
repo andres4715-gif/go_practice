d7e31b3b2c1290f63ac0a8a724613e0f7b4c9106
ariosmon <ariosmon@cisco.com>
2026-03-09 17:23:15 -0500

    adding new data for slice
    Signed-off-by: ariosmon <ariosmon@cisco.com>

--- basic_learning/slice/slicePractice/main.go
+++ basic_learning/slice/slicePractice/main.go
@@ -6,12 +6,20 @@ import (
 )
 
 func main() {
-	myArray := []int{1, 2, 3, 4, 5}
-	result, err := practica.RetornandoArray(myArray)
+	myArrayConDatos := []int{1, 2, 3, 4, 5} // Para ejemplos con array con datos
+	// myArrayVacio := []int{} // Para ejemplos con array sin datos
+	result, err := practica.RetornandoArrayConDatos(myArrayConDatos)
 
 	if err != nil {
 		fmt.Println("Hubo un error:", err)
 	} else {
-		fmt.Println("El resultado es: ", result)
+		fmt.Println("El resultado con los datos agregados es: ", result)
+	}
+
+	eliminandoDato, eliminandoDatoError := practica.RetornandoArrayConDatosEliminados(myArrayConDatos)
+	if eliminandoDatoError != nil {
+		fmt.Println("Hubo un error:", eliminandoDato)
+	} else {
+		fmt.Println("El resultado sin el dato eliminado es: ", eliminandoDato)
 	}
 }
--- basic_learning/slice/slicePractice/practica/slicePracticaAgregandoDatos.go
+++ basic_learning/slice/slicePractice/practica/slicePracticaAgregandoDatos.go
@@ -2,7 +2,7 @@ package practica
 
 import "errors"
 
-func RetornandoArray(dataSlice []int) ([]int, error) {
+func RetornandoArrayConDatos(dataSlice []int) ([]int, error) {
 	if len(dataSlice) == 0  {
 		return nil, errors.New("💥💥💥 The slice is empty")
 	}
@@ -10,4 +10,16 @@ func RetornandoArray(dataSlice []int) ([]int, error) {
 	newData := append(dataSlice, 6, 7, 8, 9, 10)
 	dataSlice = newData
 	return dataSlice, nil
+}
+
+func RetornandoArrayConDatosEliminados(dataSlice []int) ([]int , error) {
+	if len(dataSlice) == 0 {
+		return nil, errors.New("💥💥💥 The slice is empty")
+	}
+
+	newData1 := append(dataSlice, 6, 7, 8, 9, 10)
+	dataSlice = newData1
+	dataSlice1 := append(dataSlice[:1], dataSlice[2:]...)
+	
+	return dataSlice1, nil
 }
