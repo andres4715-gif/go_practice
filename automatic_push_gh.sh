1db6d955549488cd6ef9e9207f6e520ec47ec8da
ariosmon <ariosmon@cisco.com>
2026-07-30 23:05:39 -0500

    adding new scenario

--- automatic_push_gh.sh
+++ automatic_push_gh.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-CURRENT_BRANCH=git branch --show-current
+CURRENT_BRANCH=$(git branch --show-current)
+FILE_TO_ADD=automatic_push_gh.sh
 
-git add -A
+echo "${CURRENT_BRANCH}"
+
+if [ -z "$1" ]; then
+    echo "Error: You should add a commit message"
+    exit 1
+fi
+
+git add $FILE_TO_ADD
 git commit -m "$1"
-git push origin ${CURRENT_BRANCH}
+git push origin "${CURRENT_BRANCH}"
