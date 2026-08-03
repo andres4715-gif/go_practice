684591bf5a9e566a4e1772182484458876b24585
ariosmon <ariosmon@cisco.com>
2026-08-03 11:32:28 -0500

    adding pointer5 example

---
+++ basic_learning/pointers/pointer5/example5.go
@@ -0,0 +1,19 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+type Generic struct {
+	Name string
+}
+
+func (s *Generic) something() {
+	fmt.Println("The employee name is:", s.Name)
+}
+
+func main() {
+	g := &Generic{Name: "Andres"}
+	g.something()
+}
