c958b1571814f1bebff837c337016b90ddc26b12
ariosmon <ariosmon@cisco.com>
2026-08-03 22:46:52 -0500

    adding some testing example

---
+++ basic_learning/loops/for_with_slice/example1.go
@@ -0,0 +1,21 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+
+func contains(siblings []string, value string) bool {
+	for _, element := range siblings {
+		if element == value {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	myData := contains([]string{"Andres", "Daniel", "Liliana"}, "Carlos") // Must print false, cos "Carlos" does not exist in slice siblings
+	fmt.Println(myData)
+}
