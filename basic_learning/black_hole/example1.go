b0d0c2fbe5da803c4a18f1f51e921928d955f4ff
ariosmon <ariosmon@cisco.com>
2026-08-05 15:56:05 -0500

    adding new name function

--- basic_learning/black_hole/example1.go
+++ basic_learning/black_hole/example1.go
@@ -19,7 +19,7 @@ type house struct {
 	Type       string
 }
 
-func carData(model string, version string, brand string, year int64, price int64) (car, error) {
+func carDataConstructor(model string, version string, brand string, year int64, price int64) (car, error) {
 	if model == "" || version == "" || brand == "" || year <= 0 || price <= 0 {
 		return car{}, fmt.Errorf("Missing car data required data or negative data")
 	}
@@ -34,7 +34,7 @@ func carData(model string, version string, brand string, year int64, price int64
 	return newCar, nil
 }
 
-func houseData(city string, department string, price int64, myType string) (house, error) {
+func houseDataConstructor(city string, department string, price int64, myType string) (house, error) {
 	if city == "" || department == "" || price <= 0 || myType == "" || myType != "house" && myType != "apartment" {
 		return house{}, fmt.Errorf("Missing house mandatory data or some errors")
 	}
@@ -49,13 +49,13 @@ func houseData(city string, department string, price int64, myType string) (hous
 }
 
 func main() {
-	myCar, err := carData("3", "Gran turing", "Mazda", 2017, 30000)
+	myCar, err := carDataConstructor("3", "Gran turing", "Mazda", 2017, 30000)
 	if err != nil {
 		log.Fatalf("🚨 No valid operation: %s", err)
 	}
 	fmt.Printf("Register car: %s %s, Price: $%v\n", myCar.Brand, myCar.Model, myCar.Price)
 
-	myNewHouse, err := houseData("Caldas", "Antioquia", 1000, "apartment")
+	myNewHouse, err := houseDataConstructor("Caldas", "Antioquia", 1000, "apartment")
 	if err != nil {
 		log.Fatalf("Is not possible to register %s", err)
 	}
