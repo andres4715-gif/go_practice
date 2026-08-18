11884f14541dfcb2a70c06cf5798af121fd46832
ariosmon <ariosmon@cisco.com>
2026-08-18 14:50:57 -0500

    pointers example

---
+++ basic_learning/pointers/pointer6/pointerExample.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 45
+	z := &x // memory address
+	fmt.Println("🚚 The memory address is: ", z)
+	fmt.Println("🚚 The memory address is: ", *z) // use * to see the pointer value
+
+	// What about changing the z value
+	*z = 5656
+	fmt.Println("😎 The new z value is: ", *z)
+}
+
+
+/*
+Output:
+ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
+🚚 The memory address is:  0x49cc2e5da0e0
+🚚 The memory address is:  45
+😎 The new z value is:  5656
+*/
