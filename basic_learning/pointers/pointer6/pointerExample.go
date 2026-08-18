b5054892bed3d339292dc4dfe786a24d2135df2f
ariosmon <ariosmon@cisco.com>
2026-08-18 15:33:26 -0500

    adding some pointer practice

--- basic_learning/pointers/pointer6/pointerExample.go
+++ basic_learning/pointers/pointer6/pointerExample.go
@@ -2,17 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
-	x := 45
-	z := &x // memory address
-	fmt.Println("🚚 The memory address is: ", z)
-	fmt.Println("🚚 The memory address is: ", *z) // use * to see the pointer value
+// func main() {
+// 	x := 45
+// 	z := &x // memory address
+// 	fmt.Println("🚚 The memory address is: ", z)
+// 	fmt.Println("🚚 The memory address is: ", *z) // use * to see the pointer value
 
-	// What about changing the z value
-	*z = 5656
-	fmt.Println("😎 The new z value is: ", *z)
-}
-
+// 	// What about changing the z value
+// 	*z = 5656
+// 	fmt.Println("😎 The new z value is: ", *z)
+// }
 
 /*
 Output:
@@ -21,3 +20,13 @@ Output:
 🚚 The memory address is:  45
 😎 The new z value is:  5656
 */
+
+// Practicing pointers:
+func main() {
+	a := 34
+	b := &a
+
+	fmt.Println(a)  // Printing the value
+	fmt.Println(b)  // Printing the memory address
+	fmt.Println(*b) // Printing the memory address value
+}
